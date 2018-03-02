e6f7b0bc315bc73f3fcd8f66925ea8fb5485ad9d
Rene K. Mueller <spiritdude@gmail.com>
2018-03-02 17:04:54 +0100

    i2c: dynamic probing of devices

--- modules/i2c/i2c.lua
+++ modules/i2c/i2c.lua
@@ -11,7 +11,6 @@
 
 local I2C = require('periphery').I2C
 
-
 i2c = {
    DEV_PREFIX = I2C_DEV_PREFIX or "/dev/i2c-",
    --HW0 = "/dev/i2c-0",   -- these will be autoset
@@ -21,21 +20,22 @@ i2c = {
    RECEIVER = 1
 }
 
-if not file.exists(i2c.DEV_PREFIX..0) then
-   _syslog.print(_syslog.INFO,"i2c: no interface found")
-else
-   local i = 0
-   local o = ""
-   for d=0,7 do 
-      if not file.exists(i2c.DEV_PREFIX..d) then
-         break
-      end
-      o = o .. (i>0 and " " or "") .. i2c.DEV_PREFIX .. i
-      i2c['HW'..i] = i2c.DEV_PREFIX .. i
-      i = i+1
-      i2c._count = i
+-- we probe the i2c devices and dynamically set i2c.HW[0..x] = i2c.DEV_PREFIX..d (e.g. i2c.HW0 = "/dev/i2c-0")
+local i = 0
+local o = ""
+for d=0,7 do 
+   if not file.exists(i2c.DEV_PREFIX..d) then
+      break
    end
+   o = o .. (i>0 and " " or "") .. i2c.DEV_PREFIX .. i
+   i2c['HW'..i] = i2c.DEV_PREFIX .. i
+   i = i+1
+   i2c._count = i
+end
+if i>0 then
    _syslog.print(_syslog.INFO,"i2c: "..i.." interface(s) found: "..o)
+else
+   _syslog.print(_syslog.INFO,"i2c: no interface found")
 end
 
 i2c.setup = function(id,sda,scl,sp)   -- Initialize the IC module.
